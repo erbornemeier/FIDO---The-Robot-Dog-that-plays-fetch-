c65affcb59627b5d78390b2e8a2d4432d3b9add7
tlucas96 <tlucas@mymail.mines.edu>
2018-04-28 17:36:01 -0600

    commented i2c out wrote motor code

--- Arduino/src/MotorControl/MotorControl.ino
+++ Arduino/src/MotorControl/MotorControl.ino
@@ -3,44 +3,41 @@
 const int addr = 0x43;
 
 enum RECEIVE_ACTIONS {BALL_DATA};
+enum WHEEL_SIDE {LEFT, RIGHT};  //controls both right motors and both left motors
+
+
+int ENA=5; 
+int IN1=6;
+int IN2=7;
+int ENB=11; 
+int IN3=8;
+int IN4=9;
 
 void setup() {
-    Wire.begin(addr);
-    Wire.onReceive(receiveData);
-    Wire.onRequest(nothing);
-=======
-//define IO pin
-#define ENB 6
-#define IN3 9
-#define IN4 11
+    //Wire.begin(addr);
+    //Wire.onReceive(receiveData);
+    //Wire.onRequest(nothing);
+//=======
 
-const int NUM_WHEELS = 4;
-const int WHEEL_PINS = {};
+volatile int ball_size;
+volatile int ball_offset;
 
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  digitalWrite(ENA,HIGH);  
+  digitalWrite(ENB,HIGH);      
 
-void setup() {
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  digitalWrite(ENB, HIGH);  
 }
-
 void loop() {
-  //digitalWrite(IN3, LOW);
-  //digitalWrite(IN4, HIGH);
-  //delay(500);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  //delay(500);
-  //digitalWrite(IN3, HIGH);
-  //digitalWrite(IN4, LOW);
-  //delay(500);
-  //digitalWrite(IN3, HIGH);
-  //digitalWrite(IN4, LOW);
-  //delay(500);
-  
+    setMotor(LEFT, 128);
+    setMotor(RIGHT, -128);  
 }
 
+/*
 void receiveData(int num_bytes) {
 
     int cmd = Wire.read();
@@ -48,8 +45,8 @@ void receiveData(int num_bytes) {
     
     if (num_bytes == bytes + 2) {
         if (cmd == BALL_DATA) {
-          int size = i2cGetInt();
-          int offset = i2cGetInt();
+          ball_size = i2cGetInt();
+          ball_offset = i2cGetInt();
           //TODO: change above to volatile int objects for feedback loop
         }
         else {
@@ -63,6 +60,7 @@ void receiveData(int num_bytes) {
     
 }
 
+
 void nothing(){}
 
 // Reads the next 2 bytes from the i2c bus and splices them together to make a signed 16-bit integer.
@@ -73,3 +71,36 @@ int i2cGetInt() {
 void dumpData() {
     while (Wire.available()) Wire.read();
 }
+*/
+
+void setMotor(int WHEEL_SIDE, int speed){
+    switch (WHEEL_SIDE) {
+        case LEFT:
+            if (speed < 0) {
+                analogWrite(ENA, -speed);
+                digitalWrite(IN1,LOW);    //left wheels backup  
+                digitalWrite(IN2,HIGH);    
+            }
+            else {
+                analogWrite(ENA, speed);
+                digitalWrite(IN1,HIGH);      
+                digitalWrite(IN2,LOW);
+            }
+        case RIGHT:
+            if (speed < 0) {
+                analogWrite(ENB, -speed);
+                digitalWrite(IN3,HIGH);    //right wheels backup  
+                digitalWrite(IN4,LOW);    
+            }
+            else {
+                analogWrite(ENB, speed);
+                digitalWrite(IN3,LOW);      
+                digitalWrite(IN4,HIGH);
+            }
+             
+    }
+   
+}
+
+
+
