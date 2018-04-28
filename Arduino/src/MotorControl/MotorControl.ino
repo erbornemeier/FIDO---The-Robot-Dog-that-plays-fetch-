1ec370ac3d890c25381d4c240c85b1e57bbd5242
Stephen Agee <sagee@mines.edu>
2018-04-28 13:36:05 -0600

    Fixed arduino starter code to the right robot

--- Arduino/src/MotorControl/MotorControl.ino
+++ Arduino/src/MotorControl/MotorControl.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+//define IO pin
+#define ENB 6
+#define IN3 9
+#define IN4 11
 
+const int NUM_WHEELS = 4;
+const int WHEEL_PINS = {};
+
+
+void setup() {
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  digitalWrite(ENB, HIGH);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //digitalWrite(IN3, LOW);
+  //digitalWrite(IN4, HIGH);
+  //delay(500);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  //delay(500);
+  //digitalWrite(IN3, HIGH);
+  //digitalWrite(IN4, LOW);
+  //delay(500);
+  //digitalWrite(IN3, HIGH);
+  //digitalWrite(IN4, LOW);
+  //delay(500);
+  
 }
